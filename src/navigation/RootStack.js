ecb92ee4c1581bca0ca4828eef1cccdcd47bfef3
Ally Tran <h.anhtr15@gmail.com>
2022-08-10 17:32:07 +0700

    chore: update tab icon color

--- src/navigation/RootStack.js
+++ src/navigation/RootStack.js
@@ -1,25 +1,33 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { FontAwesome5 } from "@expo/vector-icons";
-import React, { useState } from "react";
-import { Octicons } from "@expo/vector-icons";
+import React, { useState } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Colors, useTheme } from 'react-native-paper';
+import { FontAwesome5 } from '@expo/vector-icons';
 
-import HomeScreen from "../screens/home/HomeScreen";
-import Resources from "../screens/Resources/Resources";
+import HomeScreen from '../screens/home/HomeScreen';
+import Resources from '../screens/Resources/Resources';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const tabBarConfig = {
-  ReportCard: { icon: "flag", label: "Report Card" },
-  Resources: { icon: "book", label: "Resources" },
-  Revision: { icon: "calendar", label: "Revision" },
-  Helplines: { icon: "phone", label: "Helplines" },
-  Marks: { icon: "list-alt", label: "Marks" },
-  Progress: { icon: "chalkboard-teacher", label: "Progress" },
+  ReportCard: { icon: 'flag', label: 'Report Card' },
+  Resources: { icon: 'book', label: 'Resources' },
+  Revision: { icon: 'calendar', label: 'Revision' },
+  Helplines: { icon: 'phone', label: 'Helplines' },
+  Marks: { icon: 'list-alt', label: 'Marks' },
+  Progress: { icon: 'chalkboard-teacher', label: 'Progress' },
+};
+
+const ROLES = {
+  STUDENT: 'STUDENT',
+  TEACHER: 'TEACHER',
+  PARENT: 'PARENT',
 };
 
 export default function RootStack() {
+  const { colors } = useTheme();
+
   const screenOptions = ({ route }) => ({
     title: tabBarConfig[route.name].label,
     tabBarIcon: ({ focused, color, size }) => {
@@ -27,44 +35,46 @@ export default function RootStack() {
 
       return <FontAwesome5 name={iconName} size={20} color={color} />;
     },
-    tabBarActiveTintColor: "#fd3667",
-    tabBarInactiveTintColor: "#473f97",
-    headerStyle: { backgroundColor: "#473f97", borderBottomStartRadius: 30 },
-    headerTintColor: "white",
+    tabBarActiveTintColor: colors.primary,
+    tabBarInactiveTintColor: Colors.grey500,
+    headerStyle: {
+      backgroundColor: colors.secondary,
+      borderBottomStartRadius: 30,
+    },
+    headerTintColor: 'white',
   });
-  const [statusTabBar, setStatusTabBar] = useState(0);
+  const [statusTabBar, setStatusTabBar] = useState(ROLES.STUDENT);
 
   return (
     <Tab.Navigator screenOptions={screenOptions}>
-      {statusTabBar == 2 ? (
-        <>
-          <Tab.Screen name="ReportCard" component={HomeScreen} />
-          <Tab.Screen name="Helplines" component={HomeScreen} />
-        </>
-      ) : (
+      {
+        (statusTabBar === ROLES.TEACHER && (
+          <>
+            <Tab.Screen
+              name="Marks"
+              component={MoreStack}
+              options={{ headerShown: false }}
+            />
+            <Tab.Screen
+              name="Progress"
+              component={MoreStack}
+              options={{ headerShown: false }}
+            />
+          </>
+        ))
+      }
+      {[ROLES.STUDENT, ROLES.PARENT].includes(statusTabBar) && (
+        <Tab.Screen name="ReportCard" component={HomeScreen} />
+      )}
+      {[ROLES.STUDENT, ROLES.TEACHER].includes(statusTabBar) && (
         <>
           <Tab.Screen name="Resources" component={Resources} />
           <Tab.Screen name="Revision" component={HomeScreen} />
-          {statusTabBar == 1 ? (
-            <>
-              <Tab.Screen
-                name="Marks"
-                component={MoreStack}
-                options={{ headerShown: false }}
-              />
-              <Tab.Screen
-                name="Progress"
-                component={MoreStack}
-                options={{ headerShown: false }}
-              />
-            </>
-          ) : (
-            <>
-              <Tab.Screen name="ReportCard" component={HomeScreen} />
-              <Tab.Screen name="Helplines" component={HomeScreen} />
-            </>
-          )}
         </>
+      )}
+
+      {[ROLES.STUDENT, ROLES.PARENT].includes(statusTabBar) && (
+        <Tab.Screen name="Helplines" component={HomeScreen} />
       )}
     </Tab.Navigator>
   );
@@ -72,8 +82,8 @@ export default function RootStack() {
 
 function MoreStack() {
   const screenOptions = ({ route }) => ({
-    headerStyle: { backgroundColor: "#fd3667" },
-    headerTintColor: "white",
+    headerStyle: { backgroundColor: '#fd3667' },
+    headerTintColor: 'white',
   });
 
   return (
@@ -81,12 +91,12 @@ function MoreStack() {
       <Stack.Screen
         name="MoreScreen"
         component={HomeScreen}
-        options={{ title: "More" }}
+        options={{ title: 'More' }}
       />
       <Stack.Screen
         name="Account"
         component={HomeScreen}
-        options={{ title: "Account" }}
+        options={{ title: 'Account' }}
       />
     </Stack.Navigator>
   );
