e0e815bb55d2fd3d189df87dfb3d90357a953be8
Nguyen Tien Duong <ntd10102000@gmail.com>
2022-08-14 22:05:44 +0700

    loading TermExem

--- src/screens/marks/components/TermExams.jsx
+++ src/screens/marks/components/TermExams.jsx
@@ -1,21 +1,23 @@
-import _ from 'lodash';
-import { useEffect, useState } from 'react';
-import { ScrollView } from 'react-native';
-import { List } from 'react-native-paper';
+import _ from "lodash";
+import { useEffect, useState } from "react";
+import { ScrollView } from "react-native";
+import { List } from "react-native-paper";
 
-import { gradeAPI } from '../../../apis';
-import { useToken } from '../../../hooks/useToken';
-import StudentGradeItem from './StudentGradeItem';
+import { gradeAPI } from "../../../apis";
+import { useToken } from "../../../hooks/useToken";
+import StudentGradeItem from "./StudentGradeItem";
+import ContentLoader from "react-native-easy-content-loader";
 
 export const examNameMap = {
-  ASSIGNMENT: 'Assignment',
-  MIDDLE: 'Midterm',
-  FINAL: 'Final',
+  ASSIGNMENT: "Assignment",
+  MIDDLE: "Midterm",
+  FINAL: "Final",
 };
 
 export default function TermExams({ classSubject, students, term }) {
   const [exams, setExams] = useState([]);
   const [token] = useToken();
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     getExamGrades();
@@ -30,14 +32,15 @@ export default function TermExams({ classSubject, students, term }) {
     const { data } = await gradeAPI.getByClassSubject(token, params);
     const { payload } = data;
     setExams(payload);
+    setLoading(false);
   };
 
   const renderExams = (exam) => {
     const mergedStudents = _.merge(
-      _.keyBy(exam.grades, 'id'),
-      _.keyBy(students, 'id')
+      _.keyBy(exam.grades, "id"),
+      _.keyBy(students, "id")
     );
-    const studentsWithGrades = _.sortBy(_.values(mergedStudents), 'name');
+    const studentsWithGrades = _.sortBy(_.values(mergedStudents), "name");
 
     exam.studentGrades = studentsWithGrades;
     exam.studentCount = studentsWithGrades.length;
@@ -62,7 +65,25 @@ export default function TermExams({ classSubject, students, term }) {
 
   return (
     <ScrollView>
-      <List.AccordionGroup>{exams.map(renderExams)}</List.AccordionGroup>
+      {isLoading ? (
+        <View
+          style={{
+            paddingRight: 16,
+            backgroundColor: "#fff",
+            marginTop: 10,
+            paddingVertical: 16,
+          }}
+        >
+          <ContentLoader
+            tHeight={72}
+            tWidth={SCREEN_WIDTH - 48}
+            pRows={0}
+            titleStyles={{ borderRadius: 12 }}
+          />
+        </View>
+      ) : (
+        <List.AccordionGroup>{exams.map(renderExams)}</List.AccordionGroup>
+      )}
     </ScrollView>
   );
 }
