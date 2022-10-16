f3b2b138e7fd6eadd3f6ccb26793ea53b181b23a
Ally Tran <h.anhtr15@gmail.com>
2022-10-16 22:56:14 +0700

    fix: old messages flash on enter screen

--- src/hooks/useMessages.js
+++ src/hooks/useMessages.js
@@ -37,6 +37,9 @@ export default function useMessages(otherUser) {
       dispatch(setMessages(data));
     });
 
-    return () => unsubscribe();
+    return () => {
+      unsubscribe();
+      dispatch(setMessages([]));
+    };
   }, [user, otherUser]);
 }
