6f9e6eb59b5c7912f37bfe2addf63a6fc2350233
Ally Tran <h.anhtr15@gmail.com>
2022-10-17 19:21:47 +0700

    chore: backend host

--- src/apis/configs/baseAxios.js
+++ src/apis/configs/baseAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseAxios = axios.create({
-  baseURL: "http://18.140.1.121/api/",
+  baseURL: "http://3.0.19.125/api/",
   // baseURL: 'http://localhost:8000/api/',
 });
 
